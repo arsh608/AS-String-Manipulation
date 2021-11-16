681e60a2d4c073b2b53401ff468e658612532a44
Arsh Al Aman <93732057+arsh608@users.noreply.github.com>
2021-11-16 22:04:51 +0500

    Add files via upload

---
+++ donuts/donuts.vb
@@ -0,0 +1,19 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim D As Integer
+
+
+        D = 0
+
+        Console.Write("Enter Number of DONUTS: ")
+        D = Console.ReadLine
+        If D >= 0 And D < 10 Then
+            Console.Write(D & " DONUTS are ordered")
+        ElseIf D >= 10 Then
+            Console.Write("Many DONUTS are ordered")
+        End If
+        Console.ReadKey()
+    End Sub
+
+End Module
