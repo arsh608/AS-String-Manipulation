ba371ef7d71ec90f8ceb4d286b30d1f1ebfd05a8
Arsh Al Aman <93732057+arsh608@users.noreply.github.com>
2021-11-16 22:10:35 +0500

    Add files via upload

---
+++ both ends/both ends.vb
@@ -0,0 +1,25 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim str1, str2, str3 As String
+
+        str1 = ""
+        str2 = ""
+        str3 = ""
+
+        Console.Write("Enter the string: ")
+        str1 = Console.ReadLine
+
+        If Len(str1) <= 2 Then
+            Console.Write("The answer is: " & str1)
+        Else
+            str2 = Left(str1, 2)
+            str3 = Right(str1, 2)
+            Console.Write("The answer is: " & str2 & str3)
+
+        End If
+        Console.ReadKey()
+
+    End Sub
+
+End Module
