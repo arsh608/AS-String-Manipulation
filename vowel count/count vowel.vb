df120fb59e32608c54816ff5dacaa4e9a5d63b3d
Arsh Al Aman <93732057+arsh608@users.noreply.github.com>
2021-11-16 22:08:54 +0500

    Add files via upload

---
+++ vowel count/count vowel.vb
@@ -0,0 +1,29 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim str1, V As String
+        Dim thischar As Char
+        Dim count, VowelCount As Integer
+
+        str1 = ""
+        thischar = ""
+        VowelCount = 0
+        count = 0
+        V = "AEIOU"
+
+        Console.Write("Enter the string to count Vowels: ")
+        str1 = UCase(Console.ReadLine())
+
+        For count = 1 To Len(str1)
+            thischar = Mid(str1, count, 1)
+            If InStr(V, thischar) > 0 Then
+                VowelCount = VowelCount + 1
+            End If
+        Next
+
+        Console.WriteLine("Vowels in the given string are= " & VowelCount)
+        Console.ReadKey()
+
+    End Sub
+
+End Module
